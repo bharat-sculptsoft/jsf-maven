2e882ba44e7f839d462c10f4c4eacb5e0c95c62a
mehulrss <mehul.rathod@sculptsoft.com>
2023-08-04 18:01:04 +0530

    csrf implementation

--- src/main/java/com/ss/message/Constant.java
+++ src/main/java/com/ss/message/Constant.java
@@ -1,5 +1,7 @@
 package com.ss.message;
 
+import com.sun.faces.RIConstants;
+
 /*this class is used to get the internal constant value
 */
 public class Constant {
@@ -13,6 +15,7 @@ public class Constant {
 	public static final String SUCCESS_PAGE_REDIRECT_URL = "success?faces-redirect=true";
 	public static final String CONFIRM_PAGE_REDIRECT_URL = "success?faces-redirect=true";
 	public static final String LOGIN_PAGE_REDIRECT_URL = "login?faces-redirect=true";
+	public static final String WELCOME_PAGE_REDIRECT_URL = "welcome?faces-redirect=true";
 
 	public static final String EMAIL_REGEX = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}";
 
@@ -20,4 +23,7 @@ public class Constant {
 	
 	public static final String EMAIL = "email";
 	public static final String PASSWORD = "password";
+
+	public static final String SESSION_TOKEN_KEY = RIConstants.FACES_PREFIX + "TOKEN";
+	public static final String REQUEST_TOKEN_KEY = "javax.faces.Token";
 }
--- src/main/java/com/ss/user/web/AuthenticationBean.java
+++ src/main/java/com/ss/user/web/AuthenticationBean.java
@@ -1,18 +1,18 @@
 package com.ss.user.web;
 
 import java.io.Serializable;
-
+import java.util.Map;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-
 import com.ss.message.Constant;
 import com.ss.user.service.UserService;
-
+import com.ss.util.FileReaderWriterUtil;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -22,41 +22,52 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthenticationBean implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	@NotBlank(message = "{validation.authenticationBean.email.NotBlank}")
-	@Email(regexp = Constant.EMAIL_REGEX, message ="{validation.authenticationBean.email.Regex}")
-	private String email;
+  @NotBlank(message = "{validation.authenticationBean.email.NotBlank}")
+  @Email(regexp = Constant.EMAIL_REGEX, message ="{validation.authenticationBean.email.Regex}")
+  private String email;
 
-	@NotBlank(message = "{validation.authenticationBean.password.NotBlank}")
-	private String password;
+  @NotBlank(message = "{validation.authenticationBean.password.NotBlank}")
+  private String password;
 
-	@ManagedProperty(value = "#{userService}")
-	private UserService userService;
+  @ManagedProperty(value = "#{userService}")
+  private UserService userService;
 
-	public String login() {
-		try {
-			if (userService.authenticate(email, password)) {
-				return Constant.SUCCESS_PAGE_REDIRECT_URL;
-			}
-		} catch (Exception e) {
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), null));
-		}
-		return null;
+  public String login() {
+    try {
+      if (userService.authenticate(email, password)) {
+        return Constant.SUCCESS_PAGE_REDIRECT_URL;
+      }
+    } catch (Exception e) {
+      FacesContext.getCurrentInstance().addMessage(null,
+          new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), null));
+    }
+    return null;
+  }
 
-	}
+  public String logout() {
+    try {
+      userService.logoutUser();
 
-	public String logout() {
-		try {
-			userService.logoutUser();
-			// Perform any additional cleanup or logout logic
-			return Constant.LOGIN_PAGE_REDIRECT_URL;
-		} catch (Exception e) {
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), null));
-		}
-		return null;
-	}
+      //delete the request details from the file
+      deleteUserRequestDetails();
 
+      // Perform any additional cleanup or logout logic
+      return Constant.WELCOME_PAGE_REDIRECT_URL;
+    } catch (Exception e) {
+      FacesContext.getCurrentInstance().addMessage(null,
+          new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), null));
+    }
+    return null;
+  }
+
+  private void deleteUserRequestDetails() throws Exception {
+    FacesContext context = FacesContext.getCurrentInstance();
+    ExternalContext externalContext = context.getExternalContext();
+    Map<String, String> requestParameterMap = externalContext.getRequestParameterMap();
+    String requestToken = requestParameterMap.get(Constant.REQUEST_TOKEN_KEY);
+  
+    FileReaderWriterUtil.deleteRequestDetails(requestToken);
+  }
 }
