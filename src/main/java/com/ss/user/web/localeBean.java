1942e7f539f04561fa7e9ae9a8c24a2b0b3d3117
Bharat Kadchha <bharat.sculptsoft@gmail.com>
2023-08-10 17:47:55 +0530

    Globle I18N & JWTFilter Response & Rest API

---
+++ src/main/java/com/ss/user/web/localeBean.java
@@ -0,0 +1,35 @@
+package com.ss.user.web;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class localeBean implements Serializable {
+
+	private Locale locale;
+
+	@PostConstruct
+	public void init() {
+		locale =FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();;
+	}
+
+	public Locale getLocale() {
+		return locale;
+	}
+
+	public String getLanguage() {
+		return locale.getLanguage();
+	}
+
+	public void setLanguage(String language) {
+		locale = new Locale(language);
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+	}
+
+}
